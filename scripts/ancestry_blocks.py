39299570e5e01b100e74d60682638be3a67b9c11
DomNelson <nelson.dominic@gmail.com>
2017-05-26 13:41:49 +0100

    Create class for analyzing ancestry tracts associated with an msprime TreeSequence

--- scripts/ancestry_blocks.py
+++ scripts/ancestry_blocks.py
@@ -2,15 +2,85 @@ import msprime
 import numpy as np
 import sys, os
 import math
+import attr
+from collections import defaultdict, Counter
+
+def counter_to_hist(counter):
+    """ Converts a Counter object to a numpy histogram """
+    ##TODO: Could do this without expanding the list of items
+    return np.histogram(list(counter.elements()))
+
+
+@attr.s
+class TreeAncestry:
+    TreeSequence = attr.ib()
+    t_divergence = attr.ib()
+    t_admixture = attr.ib()
+
+    def __attrs_post_init__(self):
+        self.records = TreeSequence.records()
+        self.ancestors = self.get_ancestors()
+
+
+    def get_ancestors(self):
+        ## Collect nodes from the source populations
+        ancestry_dict = {}
+        for i, node in enumerate(ts.nodes()):
+            if self.t_divergence > node.time and self.t_admixture < node.time:
+                ancestry_dict[i] = node.population
+
+        return ancestry_dict
+
+
+    def get_ancestry_tracts(self, SparseTree):
+        """
+        Returns a dict containing the number of tracts of each ancestry
+        contained in the provided tree, along with their length
+        """
+        length = SparseTree.interval[1] - SparseTree.interval[0]
+        ancestry_tracts = defaultdict(int)
+
+        for ancestor, ancestry in self.ancestors.items():
+            try:
+                if SparseTree.is_internal(ancestor):
+                    ## Each leaf descended from an ancestor represents a
+                    ## copy of this ancestry tract
+                    num_copies = SparseTree.num_leaves(ancestor)
+                    ancestry_tracts[ancestry] += num_copies
+            except ValueError:
+                ## Raised when node is not present in tree
+                continue
+
+        return ancestry_tracts, length
+            
+
+    def bin_ancestry_tracts(self, nbins=None):
+        """ Returns a histogram of tract lengths for each ancestry"""
+        tract_lengths = defaultdict(Counter)
+        tract_length_hist = {}
+
+        for tree in self.TreeSequence.trees():
+            ancestry_tracts, length = get_ancestry_tracts(tree)
+
+            ## Update the count of tract lengths within each ancestry
+            for ancestry, num_copies in ancestry_tracts.items():
+                tract_lengths[ancestry] += {length: num_copies}
+
+        ## Convert Counter object of tract lengths to histogram
+        for ancestry, counts in tract_lengths.items():
+            tract_length_hist[ancestry] = counter_to_hist(counts)
+
+        return tract_length_hist
+
 
 admixed_sample_size = 30
 admixed_pop_size = 30
 t_div = 1000
-t_admix = 500
+t_admix = 50
 Ne = 1000
 
 ## Set parameters of admixture event
-admixed_prop = 0.3
+admixed_prop = 0.7
 A_size = admixed_sample_size * admixed_prop
 B_size = admixed_sample_size * 1 - admixed_prop
 
@@ -40,11 +110,3 @@ ts = msprime.simulate(population_configurations=population_configurations,
                         demographic_events=demographic_events,
                         recombination_rate=1e-8, length=1e4, Ne=Ne)
 
-## Collect nodes from the source populations
-ancestor_nodes = set()
-for i, node in enumerate(ts.nodes()):
-    if t_div > node.time and t_admix < node.time:
-        ancestor_nodes.add(i)
-
-## Collect ancestry tracts from one of the source populations
-tracts = [e for e in ts.edgesets() if e.parent in ancestor_nodes]
