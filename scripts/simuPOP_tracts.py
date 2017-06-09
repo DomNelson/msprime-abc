ed31a5f46332562cede4879e5c6b29410f825989
DomNelson <nelson.dominic@gmail.com>
2017-06-09 10:58:56 +0100

    Use simuPOP.pop attribute dict for pop params
    simuPOP populations have a container for arbitrary population info,
    so no need to create a separate class to hold it.

--- scripts/simuPOP_tracts.py
+++ scripts/simuPOP_tracts.py
@@ -7,7 +7,7 @@ from collections import defaultdict, Counter
 import simuPOP as sim
 
 
-def get_tracts(genotype):
+def tract_lengths(genotype):
     """ Returns tract lengths of each ancestry """
     ##TODO Make sure to split chomosome copies +t1
     genotype = np.array(genotype)
@@ -29,95 +29,83 @@ def get_tracts(genotype):
     return binned_tracts
 
 
-def get_inds_per_ancestry(n_inds, ancestry_props):
+def ancestor_counts(n_inds, ancestry_props):
     """ Returns number of inds with each ancestry, in list format """
     inds_per_ancestry = (np.array(ancestry_props) * n_inds).astype(int)
     return list(inds_per_ancestry)
 
 
-def get_genotypes(pop):
+def pop_genotypes(pop):
     return [ind.genotype() for ind in pop.individuals()]
 
 
-@attr.s
-class PopTracts:
-    n_inds = attr.ib()
-    n_sites = attr.ib()
-    ancestry_props = attr.ib()
+def initialize_pop(n_inds, n_sites, ancestry_props):
+    """
+    Initializes a simuPOP population using the provided parameters
+    """
+    inds_per_anc = ancestor_counts(n_inds, ancestry_props)
+    pop = sim.Population(size=inds_per_anc, ploidy=2, loci=n_sites)
+
+    ## Set population attributes
+    pop.dvars().n_ancestries = len(ancestry_props)
+
+    ## Set genotypes for each source population as the index of their ancestry
+    for i in range(len(ancestry_props)):
+        sim.initGenotype(pop, haplotypes=[i]*n_sites, subPops=[i])
+
+    ## Merge subpopulations
+    pop.mergeSubPops(range(pop.dvars().n_ancestries))
+
+    return pop
 
 
-    def __attrs_post_init__(self):
-        self.n_ancestries = len(self.ancestry_props)
+def evolve_pop(pop, ngens, rep=1, mutation_matrix=None):
+    ## Initialize simulator, without modifying original population
+    simu = sim.Simulator(pop, stealPops=False, rep=rep)
 
-        ## No mutations
-        self.mutation_matrix = np.identity(self.n_ancestries).tolist()
+    if mutation_matrix is None:
+        mutation_matrix = np.identity(pop.dvars().n_ancestries).tolist()
 
-        ## Initialize population
-        self.initialize_pop()
+    ## Evolve with given parameters
+    simu.evolve(
+        initOps=sim.InitSex(),
+        preOps=sim.MatrixMutator(rate=mutation_matrix),
+        matingScheme=sim.RandomMating(
+                ops=sim.Recombinator(intensity=0.1)),
+        gen=ngens
+    )
 
+    ##TODO This only returns one replicate +t1
+    newpop = simu.extract(0)
 
-    def initialize_pop(self):
-        """
-        Initializes a simuPOP population using the provided parameters
-        """
-        inds_per_anc = get_inds_per_ancestry(self.n_inds, self.ancestry_props)
-        pop = sim.Population(size=inds_per_anc, ploidy=2, loci=self.n_sites)
-
-        ## Set genotypes for each source population as the index of their ancestry
-        for i in range(len(self.ancestry_props)):
-            sim.initGenotype(pop, haplotypes=[i]*self.n_sites, subPops=[i])
-
-        ## Merge subpopulations
-        pop.mergeSubPops(range(self.n_ancestries))
-
-        self.pop = pop
+    return newpop
 
 
-    def evolve_pop(self, ngens, rep=1):
-        ## Initialize simulator, without modifying original population
-        simu = sim.Simulator(self.pop, stealPops=False, rep=rep)
+def evolve_genotypes(pop, ngens):
+    """ Evolves the population ngens, and returns genotypes """
+    ##TODO Bug?? Using this instead of calling pop_genotypes outside +t2
+    ##TODO of class gives different results, namely with mutations +t2
+    print("Deprecated - use at own risk")
+    pop = evolve_pop(pop, ngens)
 
-        ## Evolve with given parameters
-        # print("Simulating with mutation matrix", self.mutation_matrix)
-        simu.evolve(
-            initOps=sim.InitSex(),
-            preOps=sim.MatrixMutator(rate=self.mutation_matrix),
-            matingScheme=sim.RandomMating(
-                    ops=sim.Recombinator(intensity=0.1)),
-            gen=ngens
-        )
-
-        ##TODO This only returns one replicate +t1
-        newpop = simu.extract(0)
-
-        return newpop
+    return pop_genotypes(pop)
 
 
-    def evolve_genotypes(self, ngens):
-        """ Evolves the population ngens, and returns genotypes """
-        ##TODO Bug?? Using this instead of calling get_genotypes outside +t2
-        ##TODO of class gives different results, namely with mutations +t2
-        print("Deprecated - use at own risk")
-        pop2 = self.evolve_pop(ngens)
-
-        return get_genotypes(pop2)
-
-
-def get_pop_tracts(pop):
+def pop_tracts(pop):
     """ Returns binned tract lengths for the provided population """
-    genotypes = get_genotypes(pop)
-    all_tracts = copy.copy(get_tracts(genotypes[0]))
+    genotypes = pop_genotypes(pop)
+    all_tracts = copy.copy(tract_lengths(genotypes[0]))
 
     for g in genotypes[1:]:
-        for k, v in get_tracts(g).items():
+        for k, v in tract_lengths(g).items():
             all_tracts[k].update(v)
 
     return all_tracts
 
-P = PopTracts(100, 100, [0.5, 0.5])
-p_2 = P.evolve_pop(10)
-g_2 = get_genotypes(p_2)
-t_2 = get_tracts(g_2[-1])
+p = initialize_pop(1000, 50, [0.3, 0.2, 0.4, 0.1])
+p_2 = evolve_pop(p, 10)
+g_2 = pop_genotypes(p_2)
+t_2 = tract_lengths(g_2[-1])
 print(g_2[-1])
 print(t_2)
-print(get_pop_tracts(p_2))
+print(pop_tracts(p_2))
